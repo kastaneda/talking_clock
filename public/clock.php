c5979e56510ece0643cf2f5d86af45bd00176f3a
Dmitry Kolesnikov <kastaneda@gmail.com>
2022-06-08 18:57:12 +0300

    Simplify format support: only WAV

--- public/clock.php
+++ public/clock.php
@@ -20,19 +20,11 @@ $dict = json_decode(file_get_contents(APP_ROOT . '/data/words.json'), true);
 $tts = new WordFiles($dict, APP_ROOT . '/data');
 $files = $tts->getFiles($clock->getClock());
 
-if (($_GET['type'] ?? '') == 'ogg') {
-    $format = 'ogg';
-    $mime = 'audio/ogg';
-} else {
-    $format = 'wav';
-    $mime = 'audio/wav';
-}
-
-header('Content-type: ' . $mime);
+header('Content-type: audio/wav');
 header('Cache-Control: no-cache, no-store, must-revalidate, max-age=0');
 header('Cache-Control: post-check=0, pre-check=0', false);
 header('Pragma: no-cache');
 header('Expires: 01 Jan 1970 00:00:00 GMT');
 
-$command = 'sox ' . join(' ', $files) . ' -t ' . $format . ' -';
+$command = 'sox ' . join(' ', $files) . ' -t wav -';
 passthru($command);
